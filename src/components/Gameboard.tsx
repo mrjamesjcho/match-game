c05f96fb8fd7019507337889738e017bacc38e9e
James Cho <mrjamesjcho@gmail.com>
2025-02-11 20:45:28 -0800

    update tile render

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -10,24 +10,26 @@ const Tile = dynamic(() => import('./Tile'), { ssr: false });
 export default function Gameboard() {
   const [matrix, setMatrix] = useState(
     Array.from({ length: NUMBER_OF_COLS }, (_, col) =>
-      Array.from({ length: NUMBER_OF_ROWS }, (_, row) => (
-        <Tile
-          key={`${col}-${row}`}
-          col={col}
-          row={row}
-          color={getRandomBackgroundColor()}
-        />
-      ))
+      Array.from({ length: NUMBER_OF_ROWS }, (_, row) => ({
+        col,
+        row,
+        color: getRandomBackgroundColor(),
+      }))
     )
   );
+
+  const renderMatrix = () =>
+    matrix.map((col, idx) => (
+      <div key={`col-${idx}`} className="grid grid-rows-8 gap-2">
+        {col.map((cell, row) => (
+          <Tile key={`${col}-${row}`} col={idx} row={row} color={cell.color} />
+        ))}
+      </div>
+    ));
   return (
     <div className="flex w-full h-screen items-center justify-center">
       <div className="border p-2 flex items-center justify-center grid grid-cols-8 gap-2">
-        {matrix.map((col, idx) => (
-          <div key={`col-${idx}`} className="grid grid-rows-8 gap-2">
-            {col.map((cell) => cell)}
-          </div>
-        ))}
+        {renderMatrix()}
       </div>
     </div>
   );
--- src/utils/color.ts
+++ src/utils/color.ts
@@ -1,10 +1,10 @@
 export enum BackgroundColor {
-  PINK = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-pink-500',
-  CYAN = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-cyan-500',
-  BLUE = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-blue-500',
-  GREEN = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-green-500',
-  YELLOW = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-yellow-500',
-  GRAY = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-gray-500',
+  PINK = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-pink-500 tile',
+  CYAN = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-cyan-500 tile',
+  BLUE = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-blue-500 tile',
+  GREEN = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-green-500 tile',
+  YELLOW = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-yellow-500 tile',
+  GRAY = 'w-10 h-10 border col-span-1 flex items-center justify-center bg-gray-500 tile',
 }
 
 export const getRandomBackgroundColor = () => {
