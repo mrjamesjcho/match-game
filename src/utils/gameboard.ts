23885be3c0a98235b62e8204fffb1c362f4f804e
James Cho <mrjamesjcho@gmail.com>
2025-02-20 13:24:47 -0800

    update logic adding new tile color

--- src/utils/gameboard.ts
+++ src/utils/gameboard.ts
@@ -52,37 +52,62 @@ export const markTilesForDeletion = (
   });
 };
 
+// add new tiles to the top of the column where tiles were deleted
 export const addNewTiles = (prevGameboard: Gameboard) => {
   return prevGameboard.map((col, colIdx) => {
+    // if a column has tiles marked for deletion
+    // - add a new tile to the top of the column for each tile marked for deletion
+    // - update the row of each tile not marked for deletion to reflect the tiles that will be removed
     if (col.find((tile) => tile.deleted)) {
       let tilesRemoved = 0;
+      const colorsInNewColumn: string[] = [];
       const newCol = [];
       for (let rowIdx = 0; rowIdx < NUMBER_OF_ROWS + tilesRemoved; rowIdx++) {
         if (rowIdx >= NUMBER_OF_ROWS) {
+          // when setting the color of a new tile
+          // make sure it doesn't match the color of the previous two tiles
+          let newColor;
+          do {
+            newColor = getRandomBackgroundColor();
+          } while (
+            newColor === colorsInNewColumn[colorsInNewColumn.length - 1] &&
+            newColor === colorsInNewColumn[colorsInNewColumn.length - 2]
+          );
           newCol.push({
             col: colIdx,
             row: rowIdx - tilesRemoved,
-            color: getRandomBackgroundColor(),
+            color: newColor,
             selected: false,
             highlighted: false,
             deleted: false,
           });
+          colorsInNewColumn.push(newColor);
           continue;
         }
         if (col[rowIdx].deleted) {
           tilesRemoved++;
-        }
-        if (!col[rowIdx].deleted && tilesRemoved) {
+        } else if (!col[rowIdx].deleted && tilesRemoved) {
+          // update rows of tiles not marked to be deleted
+          // to reflect the tiles that will be removed
           newCol.push({
             ...col[rowIdx],
             row: col[rowIdx].row - tilesRemoved,
           });
+          colorsInNewColumn.push(col[rowIdx].color);
         } else {
           newCol.push(col[rowIdx]);
+          colorsInNewColumn.push(col[rowIdx].color);
         }
       }
       return newCol;
     }
     return col;
   });
+};
+
+export const findTilesToDelete = (
+  gameboard: Gameboard,
+  tilesToDelete: TileCoordinates[]
+): TileCoordinates[] => {
+  return [];
 };
