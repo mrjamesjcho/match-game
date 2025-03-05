f9f48c3344e2186be9cce396a4d4a2c1dfcbd344
James Cho <mrjamesjcho@gmail.com>
2025-03-04 21:54:30 -0800

    refactor check for matching tiles

--- src/utils/gameboard.ts
+++ src/utils/gameboard.ts
@@ -3,6 +3,7 @@ import {
   getRandomBackgroundColor,
   getRandomBackgroundColorWithoutMatches,
   Theme,
+  TileThemeColorNumber,
 } from './color';
 
 const NUMBER_OF_COLS = 8;
@@ -37,35 +38,34 @@ export const createGameboard = (theme: Theme) => {
 
 // set or restore selected and highlighted tiles
 export const toggleSelectedAndHighlightedTiles = (
-  selectedCol: number,
-  selectedRow: number,
   gameboard: Gameboard,
+  selectedTile: TileCoordinates,
   toggleValue: boolean
 ) => {
   const updatedNewMatrix = gameboard.map((col, colIdx) => {
     let newCol;
-    if (colIdx === selectedCol) {
+    if (colIdx === selectedTile.col) {
       newCol = col.map((tile, rowIdx) => {
-        if (rowIdx === selectedRow) {
+        if (rowIdx === selectedTile.row) {
           return { ...tile, selected: toggleValue };
         }
-        if (rowIdx === selectedRow + 1) {
+        if (rowIdx === selectedTile.row + 1) {
           return { ...tile, highlighted: toggleValue, selected: false };
         }
-        if (rowIdx === selectedRow - 1) {
+        if (rowIdx === selectedTile.row - 1) {
           return { ...tile, highlighted: toggleValue, selected: false };
         }
         return { ...tile, highlighted: false, selected: false };
       });
-    } else if (colIdx === selectedCol - 1) {
+    } else if (colIdx === selectedTile.col - 1) {
       newCol = col.map((tile, rowIdx) =>
-        rowIdx === selectedRow
+        rowIdx === selectedTile.row
           ? { ...tile, highlighted: toggleValue, selected: false }
           : { ...tile, highlighted: false, selected: false }
       );
-    } else if (colIdx === selectedCol + 1) {
+    } else if (colIdx === selectedTile.col + 1) {
       newCol = col.map((tile, rowIdx) =>
-        rowIdx === selectedRow
+        rowIdx === selectedTile.row
           ? { ...tile, highlighted: toggleValue, selected: false }
           : { ...tile, highlighted: false, selected: false }
       );
@@ -79,6 +79,141 @@ export const toggleSelectedAndHighlightedTiles = (
     return newCol ?? col;
   });
   return updatedNewMatrix;
+};
+
+export const threeOrMoreMatchingTilesInSameRow = (
+  gameboard: Gameboard,
+  tile: TileCoordinates,
+  color?: TileThemeColorNumber,
+  boundryColumn?: number
+): TileCoordinates[] => {
+  const matchingTiles: TileCoordinates[] = [];
+  const colorToMatch = color ?? gameboard[tile.col][tile.row].color;
+
+  // check to the left of col
+  let currentCol = tile.col - 1;
+  while (
+    currentCol >= 0 &&
+    gameboard[currentCol][tile.row].color === colorToMatch &&
+    (!boundryColumn || currentCol !== boundryColumn)
+  ) {
+    matchingTiles.push({ col: currentCol, row: tile.row });
+    currentCol--;
+  }
+
+  // check to the right of col
+  currentCol = tile.col + 1;
+  while (
+    currentCol < NUMBER_OF_COLS &&
+    gameboard[currentCol][tile.row].color === colorToMatch &&
+    (!boundryColumn || currentCol !== boundryColumn)
+  ) {
+    matchingTiles.push({ col: currentCol, row: tile.row });
+    currentCol++;
+  }
+
+  return matchingTiles.length >= 2
+    ? matchingTiles.concat({ col: tile.col, row: tile.row })
+    : [];
+};
+
+export const threeOrMoreMatchingTilesInSameColumn = (
+  gameboard: Gameboard,
+  tile: TileCoordinates,
+  color?: TileThemeColorNumber,
+  boundryRow?: number
+): TileCoordinates[] => {
+  const matchingTiles: TileCoordinates[] = [];
+  const colorToMatch = color ?? gameboard[tile.col][tile.row].color;
+
+  // check below row
+  let currentRow = tile.row - 1;
+  while (
+    currentRow >= 0 &&
+    gameboard[tile.col][currentRow].color === colorToMatch &&
+    (!boundryRow || currentRow !== boundryRow)
+  ) {
+    matchingTiles.push({ col: tile.col, row: currentRow });
+    currentRow--;
+  }
+
+  // check above row
+  currentRow = tile.row + 1;
+  while (
+    currentRow < NUMBER_OF_ROWS &&
+    gameboard[tile.col][currentRow].color === colorToMatch &&
+    (!boundryRow || currentRow !== boundryRow)
+  ) {
+    matchingTiles.push({ col: tile.col, row: currentRow });
+    currentRow++;
+  }
+
+  return matchingTiles.length >= 2
+    ? matchingTiles.concat({ col: tile.col, row: tile.row })
+    : [];
+};
+
+export const tilesWithThreeOrMoreMatchingTilesInSameColumnOrRowIfSelectedAndHightlightedSwap =
+  (
+    gameboard: Gameboard,
+    selectedTile: TileCoordinates,
+    highlightedTile: TileCoordinates
+  ): TileCoordinates[] => [
+    ...threeOrMoreMatchingTilesInSameColumn(
+      gameboard,
+      selectedTile,
+      gameboard[highlightedTile.col][highlightedTile.row].color,
+      highlightedTile.row
+    ),
+    ...threeOrMoreMatchingTilesInSameRow(
+      gameboard,
+      selectedTile,
+      gameboard[highlightedTile.col][highlightedTile.row].color,
+      highlightedTile.col
+    ),
+    ...threeOrMoreMatchingTilesInSameColumn(
+      gameboard,
+      highlightedTile,
+      gameboard[selectedTile.col][selectedTile.row].color,
+      selectedTile.row
+    ),
+    ...threeOrMoreMatchingTilesInSameRow(
+      gameboard,
+      highlightedTile,
+      gameboard[selectedTile.col][selectedTile.row].color,
+      selectedTile.col
+    ),
+  ];
+
+export const swapTiles = (
+  gameboard: Gameboard,
+  selectedTile: TileCoordinates,
+  highlightedTile: TileCoordinates
+): Gameboard => {
+  let newGameboard = gameboard.map((col, colIdx) =>
+    col.map((tile, rowIdx) => {
+      if (colIdx === selectedTile.col && rowIdx === selectedTile.row) {
+        return {
+          ...tile,
+          color: gameboard[highlightedTile.col][highlightedTile.row].color,
+        };
+      }
+      if (colIdx === highlightedTile.col && rowIdx === highlightedTile.row) {
+        return {
+          ...tile,
+          color: gameboard[selectedTile.col][selectedTile.row].color,
+        };
+      }
+      return tile;
+    })
+  );
+
+  newGameboard = toggleSelectedAndHighlightedTiles(
+    newGameboard,
+    selectedTile,
+    false
+  );
+  return newGameboard;
 };
 
 export const markTilesForDeletion = (
@@ -160,62 +295,6 @@ export const addNewTiles = (prevGameboard: Gameboard, theme: Theme) => {
   });
 };
 
-export const threeOrMoreMatchingTilesInSameRow = (
-  gameboard: Gameboard,
-  col: number,
-  row: number
-): TileCoordinates[] => {
-  const matchingTiles: TileCoordinates[] = [];
-  const color = gameboard[col][row].color;
-
-  // check to the left of col
-  let currentCol = col - 1;
-  while (currentCol >= 0 && gameboard[currentCol][row].color === color) {
-    matchingTiles.push({ col: currentCol, row });
-    currentCol--;
-  }
-
-  // check to the right of col
-  currentCol = col + 1;
-  while (
-    currentCol < NUMBER_OF_COLS &&
-    gameboard[currentCol][row].color === color
-  ) {
-    matchingTiles.push({ col: currentCol, row });
-    currentCol++;
-  }
-
-  return matchingTiles.length >= 2 ? matchingTiles.concat({ col, row }) : [];
-};
-
-export const threeOrMoreMatchingTilesInSameColumn = (
-  gameboard: Gameboard,
-  col: number,
-  row: number
-): TileCoordinates[] => {
-  const matchingTiles: TileCoordinates[] = [];
-  const color = gameboard[col][row].color;
-
-  // check below row
-  let currentRow = row - 1;
-  while (currentRow >= 0 && gameboard[col][currentRow].color === color) {
-    matchingTiles.push({ col, row: currentRow });
-    currentRow--;
-  }
-
-  // check above row
-  currentRow = row + 1;
-  while (
-    currentRow < NUMBER_OF_ROWS &&
-    gameboard[col][currentRow].color === color
-  ) {
-    matchingTiles.push({ col, row: currentRow });
-    currentRow++;
-  }
-
-  return matchingTiles.length >= 2 ? matchingTiles.concat({ col, row }) : [];
-};
-
 export const findAdditionalTilesToDelete = (
   gameboard: Gameboard
 ): TileCoordinates[] => {
@@ -224,15 +303,13 @@ export const findAdditionalTilesToDelete = (
   const tilesToDelete: TileCoordinates[] = [];
   gameboard.forEach((col, colIdx) => {
     col.forEach((_tile, rowIdx) => {
-      const matchingTilesInRow = threeOrMoreMatchingTilesInSameRow(
-        gameboard,
-        colIdx,
-        rowIdx
-      );
+      const matchingTilesInRow = threeOrMoreMatchingTilesInSameRow(gameboard, {
+        col: colIdx,
+        row: rowIdx,
+      });
       const matchingTilesInColumn = threeOrMoreMatchingTilesInSameColumn(
         gameboard,
-        colIdx,
-        rowIdx
+        { col: colIdx, row: rowIdx }
       );
       if (matchingTilesInRow.length) {
         tilesToDelete.push(...matchingTilesInRow);
